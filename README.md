edcaa480f9bcad14a3400200f3693c482a7795da
“TanGuangZhi” <guangzhi_tan@qq.com>
2022-03-23 16:38:03 +0800

    update README

--- README.md
+++ README.md
@@ -15,5 +15,25 @@ npm run serve
 
 ```
 
+## 网页截图展示
+
+| **首页**                                                     |                                                              |      |
+| ------------------------------------------------------------ | ------------------------------------------------------------ | ---- |
+| **![image-20220323163136634](http://picbed.tgz666.top/20220323163137.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618)** | **![image-20220323163159313](http://picbed.tgz666.top/20220323163159.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618)** |      |
+| **电影**                                                     |                                                              |      |
+| ![image-20220323163231188](http://picbed.tgz666.top/20220323163231.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) | ![image-20220323163640819](http://picbed.tgz666.top/20220323163655.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) |      |
+| **影院**                                                     |                                                              |      |
+| ![image-20220323163311331](http://picbed.tgz666.top/20220323163311.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) | ![image-20220323163325836](http://picbed.tgz666.top/20220323163325.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) |      |
+| **选座购票**                                                 | 支付界面                                                     |      |
+| ![image-20220323163353710](http://picbed.tgz666.top/20220323163353.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) | ![image-20220323163414854](http://picbed.tgz666.top/20220323163414.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) |      |
+| **其他**                                                     |                                                              |      |
+| ![image-20220323163449341](http://picbed.tgz666.top/20220323163449.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618)![image-20220323163707872](C:/Users/%E8%88%B9%E9%95%BF/AppData/Roaming/Typora/typora-user-images/image-20220323163707872.png) | ![image-20220323163506168](http://picbed.tgz666.top/20220323163506.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618)![image-20220323163520089](http://picbed.tgz666.top/20220323163520.png?roundPic/radius/25%7CimageView2/2/w/1000/h/1618) |      |
+
+
+
+## TODO
+
+\- [ ] 线上部署
+
 
 
--- src/views/FilmDetail.vue
+++ src/views/FilmDetail.vue
@@ -29,7 +29,7 @@
               </li>
               <li>
                 <p>
-                  {{ filmType }}
+                  {{ film.filmType2Details[0].filmType }}
                 </p>
               </li>
               <li>
@@ -154,13 +154,16 @@
                   </a>
                 </div>
               </li>
+
               <!-- 2 -->
-              <li>
+              <li v-for="(actor, index) in film.filmActor" :key="index">
                 <div>演员</div>
                 <div>
                   <a href="#">
                     <img
-                      :src="'http://localhost:3000/' + filmActor.actorImg"
+                      :src="
+                        'http://localhost:3000/' + film.filmActor[0].actorImg
+                      "
                       alt=""
                       style="width: 138px; height: 170px"
                     />
@@ -168,56 +171,13 @@
                 </div>
                 <div>
                   <a href="#">
-                    {{ filmActor.realName }}
+                    {{ film.filmActor[0].realName }}
                   </a>
                   <p
                     title="饰：埃迪·布洛克/毒液 Eddie Brock / Venom"
                     style="font-size: 10px"
                   >
-                    饰：{{ filmActor.roleName }}
-                  </p>
-                </div>
-              </li>
-              <!-- 3 -->
-              <li>
-                <div></div>
-                <div>
-                  <a href="#">
-                    <img src="../../public/images/actor/4.jpg" alt="" />
-                  </a>
-                </div>
-                <div>
-                  <a href="#">米歇尔·威廉姆斯</a>
-                  <p title="饰：安妮·韦英 Anne Weying">饰：安妮·韦英 An...</p>
-                </div>
-              </li>
-              <!-- 4 -->
-              <li>
-                <div></div>
-                <div>
-                  <a href="#">
-                    <img src="../../public/images/actor/5.jpg" alt="" />
-                  </a>
-                </div>
-                <div>
-                  <a href="#">里兹·阿迈德</a>
-                  <p title="饰：德雷克博士 Dr. Carlton Drake">
-                    饰：德雷克博士 D...
-                  </p>
-                </div>
-              </li>
-              <!-- 5 -->
-              <li>
-                <div></div>
-                <div>
-                  <a href="#">
-                    <img src="../../public/images/actor/2.jpg" alt="" />
-                  </a>
-                </div>
-                <div>
-                  <a href="#">斯科特·黑兹</a>
-                  <p title="饰：Security Chief Roland Treece">
-                    饰：Security Chie...
+                    饰：{{ film.filmActor[0].roleName }}
                   </p>
                 </div>
               </li>
@@ -266,72 +226,41 @@
           <div class="content_L5_1">
             <h3>热门短评</h3>
             <div class="content_L5_11">
-              <a href="#">写短评</a>
+              <a @click="judgeIsCanComment">写短评</a>
             </div>
           </div>
           <div class="content_L5_2">
             <ul>
               <!-- 1 -->
-              <li>
+              <li v-for="(comment, index) in film.comment2Details" :key="index">
                 <div class="content_L5_2_1">
                   <img
-                    v-for="(item, index) in user"
-                    v-if="index == 0"
-                    :src="'http://localhost:3000/' + item.userImg"
+                    :src="
+                      'http://localhost:3000/' +
+                      comment.userId2Details[0].userImg
+                    "
                     alt=""
                   />
                   <i class="v1"></i>
                 </div>
                 <div class="content_L5_2_2">
                   <div class="content_L5_2_2_1">
-                    <span
-                      class="content_L5_2_2_11"
-                      v-for="(item, index) in user"
-                      v-if="index == 0"
-                    >
-                      {{ item.userName }}
+                    <span>
+                      {{ comment.userId2Details[0].userName }}
                     </span>
                     <span class="content_L5_2_2_12">购</span>
                   </div>
                   <div class="content_L5_2_2_2">
                     <div>
-                      <span>11-09</span>
+                      <span>{{ comment.commentTime }}</span>
                       <i class="xin5"></i>
                     </div>
                     <div>
                       <a href="./login.html" target="blank"></a>
-                      <span>13690</span>
+                      <span>{{ comment.filmScore }}</span>
                     </div>
                   </div>
                   <p>{{ comment.content }}</p>
-                </div>
-              </li>
-              2
-              <li>
-                <div class="content_L5_2_1">
-                  <img src="../../public/images/6.jpg" alt="" />
-                  <i class="v4"></i>
-                </div>
-                <div class="content_L5_2_2">
-                  <div class="content_L5_2_2_1">
-                    <span class="content_L5_2_2_11">
-                      A名犬乐园 （定金选狗）款到发货
-                    </span>
-                    <span class="content_L5_2_2_12">购</span>
-                  </div>
-                  <div class="content_L5_2_2_2">
-                    <div>
-                      <span>11-09</span>
-                      <i class="xin5"></i>
-                    </div>
-                    <div>
-                      <a href="./login.html" target="blank"></a>
-                      <span>7792</span>
-                    </div>
-                  </div>
-                  <p>
-                    真的太震撼了，漫威从来没有让我失望，虽然好多地方我感觉像是被删减过许多吓人的画面，但是真的还是太好看，好多地方从头到尾一直在笑，最后还有一点小感人。一直都是震撼幽默的画面。真希望可以能有第二部的上映。这个毒液系列希望像蜘蛛侠钢铁侠一样出一个系列有续集，真的没有看过瘾啊。（人生第一次评价话有点多）总之，大家可以去观看，我是电影迷这一部我给11分，强烈大家去观看！
-                  </p>
                 </div>
               </li>
             </ul>
@@ -339,86 +268,13 @@
         </div>
       </article>
       <aside>
-        <div class="content_R1">
-          <div class="content_R1_1">
-            <h3>热门短评</h3>
-          </div>
-          <ul>
-            <li>
-              <div>
-                <a href="#">
-                  <img src="../../public/images/6.jpg" alt="" />
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  title="《毒液》票房破17亿，给抖森换上汤老湿的厚嘴唇，画风太诡异了"
-                >
-                  《毒液》票房破17亿，给抖森换上汤老湿的厚嘴唇，画...
-                </a>
-                <p>
-                  <span>猫眼电影</span>
-                  <i></i>
-                  <span>9914</span>
-                  <i></i>
-                  <span>8</span>
-                </p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <a href="#">
-                  <img src="../../public/images/6.jpg" alt="" />
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  title="《毒液》全球票房超《神奇女侠》，中国票房超16.8亿成最大票仓"
-                >
-                  《毒液》全球票房超《神奇女侠》，中国票房超16.8亿成...
-                </a>
-                <p>
-                  <span>猫眼电影</span>
-                  <i></i>
-                  <span>14339</span>
-                  <i></i>
-                  <span>53</span>
-                </p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <a href="#">
-                  <img src="../../public/images/6.jpg" alt="" />
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  title="钢铁侠晒图，毒液现身，还和复仇者联盟抢饭吃，是在暗示什么吗"
-                >
-                  钢铁侠晒图，毒液现身，还和复仇者联盟抢饭吃，是在暗...
-                </a>
-                <p>
-                  <span>猫眼电影</span>
-                  <i></i>
-                  <span>19774</span>
-                  <i></i>
-                  <span>8</span>
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
         <div class="content_R2">
           <div class="content_R2_1">
             <h3>相关电影</h3>
           </div>
           <dl>
             <!-- 1 -->
-            <dd v-for="film in similarFilm">
+            <!-- <dd v-for="film in similarFilm">
               <router-link
                 :to="{ path: '/FilmDetail', query: { filmId: film._id } }"
               >
@@ -433,10 +289,9 @@
                 </div>
                 <div>
                   <a href="#" title="X战警：新变种人">{{ film.filmName }}.</a>
-                  <!--                <p>暂无评分</p>-->
                 </div>
               </router-link>
-            </dd>
+            </dd> -->
           </dl>
         </div>
       </aside>
@@ -459,14 +314,9 @@ export default {
   data() {
     return {
       film: {},
-      filmType: {},
-      filmActor: {},
       userId: "",
       user: [],
-      filmTypeId: "",
       similarFilm: [],
-      comment: [],
-      filmDetail: [],
       dialogObj: { comment: false },
       toCommentPageData: {},
     };
@@ -476,35 +326,23 @@ export default {
       "queryFilmByFilmIdAsync",
       "queryFilmByFilmTypeIdAsync",
     ]),
+
+    // ## film
+    // 1.
     async queryFilmByFilmId(filmId) {
-      //console.log("filmId",filmId)
       let res = await this.queryFilmByFilmIdAsync(filmId);
-      //console.log("res",res)
       this.film = res[0];
-      //console.log("this.film",this.film)
-      this.comment = this.film.comment[0];
-      //console.log("this.comment",this.comment)
-
-      this.filmActor = this.film.filmActor[0];
-      //console.log("this.filmActor",this.filmActor)
-
-      this.userId = this.comment.userId;
-
-      this.filmType = this.film.filmType[0].filmType;
-      this.filmTypeId = this.film.filmTypeId;
-      // console.log(this.film)
+      console.log(this.film);
     },
+
+    // ## other
+    // 1. query user
     async queryUserByUserId(_id) {
       let res = await api.user.queryUserByUserIdApi(_id);
       this.user = res.data;
     },
-    async queryFilmByFilmTypeId(filmTypeId) {
-      let res = await this.queryFilmByFilmTypeIdAsync(filmTypeId);
-      this.similarFilm = res;
-      // console.log("this.similarFilm",res)
-    },
 
-    // 1.
+    // 2.
     judgeIsCanComment() {
       let loginUserInfo = localStorage.getItem("loginUserInfo");
       // console.log(loginUserInfo);
@@ -529,12 +367,10 @@ export default {
       });
     },
   },
+
   created() {
     //console.log(this.$route.query.filmId)
-    this.queryFilmByFilmId(this.$route.query.filmId).then((res) => {
-      this.queryUserByUserId(this.userId);
-      this.queryFilmByFilmTypeId(this.filmTypeId);
-    });
+    this.queryFilmByFilmId(this.$route.query.filmId);
   },
 };
 </script>
